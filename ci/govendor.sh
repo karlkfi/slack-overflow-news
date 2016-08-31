5e84f2f9a0a05d175767a6c3b8adfa8ffe88a18f
Karl Isenberg <karl@mesosphere.io>
2016-08-31 11:49:12 -0700

    Init govendor

---
+++ ci/govendor.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+project_dir="$(cd "$(dirname "${BASH_SOURCE}")/.." && pwd -P)"
+
+cd "${project_dir}"
+
+curl --fail --location --silent --show-error --verbose https://raw.githubusercontent.com/karlkfi/govendor/master/govendor.sh | bash -s -- "$@"
---
+++ main.go
@@ -0,0 +1,1 @@
+package main // import "github.com/karlkfi/slackstack"
