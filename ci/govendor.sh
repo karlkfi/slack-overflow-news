716cd124e9c37757fe6813fe2e92df76dae45bf4
Karl Isenberg <karl@mesosphere.io>
2016-08-31 12:43:31 -0700

    Vendor logrus and stackongo

--- ci/govendor.sh
+++ ci/govendor.sh
@@ -8,4 +8,30 @@ project_dir="$(cd "$(dirname "${BASH_SOURCE}")/.." && pwd -P)"
 
 cd "${project_dir}"
 
-curl --fail --location --silent --show-error https://raw.githubusercontent.com/karlkfi/govendor/master/govendor.sh | bash -s -- "$@"
+# Must be run from the go project dir TODO: validate?
+# Project dir must be mountable into docker (under $HOME on Mac) TODO: validate?
+
+# Determine the project import path from the root ImportComment
+# Optionally specify via GO_PROJECT_IMPORT
+if [ -z "${PROJECT_IMPORT:-}" ]; then
+  PROJECT_IMPORT="$(
+    docker run --rm \
+        -v "$(pwd):/project" \
+        -w '/project' \
+        golang:1.6.2-alpine \
+        go list -f '{{.ImportComment}}'
+  )"
+fi
+
+# Optionally pass in SSH key to pull dependencies from private repos
+PRIVATE_KEY_PARAM=""
+if [ -n "${PRIVATE_KEY:-}" ]; then
+  PRIVATE_KEY_PARAM="-v ${PRIVATE_KEY:-}:/root/.ssh/id_rsa"
+fi
+
+docker run --rm -i \
+       $(tty -s && echo '-t') \
+       -v "$(pwd):/go/src/${PROJECT_IMPORT}" \
+       ${PRIVATE_KEY_PARAM} \
+       -w "/go/src/${PROJECT_IMPORT}" \
+       karlkfi/govendor "$@"
--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main // import "github.com/karlkfi/slackstack"
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/laktek/Stack-on-Go/stackongo"
+)
+
+func main() {
+	site := "stackoverflow"
+	session := stackongo.NewSession(site)
+
+	_, err := session.Info()
+	if err != nil {
+		log.Fatalf("Failed to retrieve %s session info: %v", site, err)
+	}
+}
